0f435f985605f8edc5eec055f59b8f257dc7c296
khalid alasmary <khalid@alasmary.com>
2020-04-01 23:44:42 +0300

    create count up component and fetch global cases number from api

--- src/components/GlobalCases.js
+++ src/components/GlobalCases.js
@@ -1,23 +1,49 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import CountUp from '../components/UIElements/CountUp';
 import './GlobalCases.css';
 
 export default () => {
+  const [confirmed, setConirmed] = useState(0);
+  const [recovered, setRecovered] = useState(0);
+  const [deaths, setDeaths] = useState(0);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('https://covid19.mathdro.id/api');
+        const data = await response.json();
+        setConirmed(data.confirmed.value);
+        setRecovered(data.recovered.value);
+        setDeaths(data.deaths.value);
+        console.log(data.confirmed.value);
+        console.log(data.recovered.value);
+        console.log(data.deaths.value);
+      } catch {
+        setError('حدث خطأ أثناء جلب البيانات');
+      }
+    })();
+  }, []);
   return (
     <section className='global'>
       <div>IMage</div>
       <div className='container'>
         <div>
           <h2>الحالات المسجلة</h2>
-          <div className='number-circle confirmed'>1000</div>
+          <div className='number-circle confirmed'>
+            <CountUp target={confirmed} speed={200} />
+          </div>
         </div>
         <div>
           <h2>حالات التعافي</h2>
-          <div className='number-circle recovered'>1000</div>
+          <div className='number-circle recovered'>
+            <CountUp target={recovered} speed={200} />
+          </div>
         </div>
         <div>
           <h2>حالات الوفاة</h2>
-          <div className='number-circle deaths'>1000</div>
+          <div className='number-circle deaths'>
+            <CountUp target={deaths} speed={200} />
+          </div>
         </div>
       </div>
     </section>
---
+++ src/components/UIElements/CountUp.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+export default ({ target, speed, start = 0 }) => {
+  const [count, setCount] = useState(start);
+  useEffect(() => {
+    let inc = target / speed;
+    if (count < target) {
+      setTimeout(() => setCount(prevCount => prevCount + inc));
+    } else {
+      setCount(target);
+    }
+  }, [count, speed, target]);
+
+  return <span>{Math.round(count)}</span>;
+};
